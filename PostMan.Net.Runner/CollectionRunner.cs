55a77e05b5f0663dcd7000d4e4a72e7ced99870c
b3b00 <olivier.duhart@gmail.com>
2023-11-09 20:53:50 +0100

    .

---
+++ PostMan.Net.Runner/CollectionRunner.cs
@@ -0,0 +1,77 @@
+using net.postman.model;
+using RestSharp;
+
+namespace PostMan.Net.Runner;
+
+public static class Extensions
+{
+    public static string Capitalize(this string s)
+    {
+        var c = s.ToLower();
+        return Char.ToUpper(c[0]) + c.Substring(1);
+    }
+}
+
+public class Runner
+{
+    public Collection Collection { get; private set; }
+    
+    public Runner(Collection collection)
+    {
+        Collection = collection;
+    }
+
+    public void Run()
+    {
+        foreach (var item in Collection.Item)
+        {
+            Run(item);
+        }
+        
+    }
+
+    private void Run(Item item)
+    {
+        if (item.IsFolder)
+        {
+            RunFolder(item);
+        }
+        else if (item.IsTestCase)
+        {
+            RunTestCase(item);
+        }
+    }
+
+    
+    private void RunFolder(Item item)
+    {
+        foreach (var subItem in item.Items)
+        {
+            Run(subItem);
+        }
+    }
+    private void RunTestCase(Item item)
+    {
+        RestClient client = new RestClient();
+        var request = new RestRequest(item.Request.Url.Raw);
+        if (Enum.TryParse<Method>(item.Request.Method.Capitalize(), out var method))
+        {
+            request.Method = method;
+        }
+        else
+        {
+            request.Method = Method.Get;
+        }
+
+        if (request.Method == Method.Post || request.Method == Method.Put)
+        {
+            request.AddBody(item.Request.Body.Raw);
+        }
+
+        var response = client.Execute<object>(request);
+        Console.WriteLine($"status : {response.StatusCode}");
+        Console.WriteLine($"response :{response.Data}");
+    }
+
+   
+}
