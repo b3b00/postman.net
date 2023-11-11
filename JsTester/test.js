529e4184566e9b85b604885cd0082323e8bc64dd
b3b00 <olivier.duhart@gmail.com>
2023-11-11 20:21:44 +0100

    more and more tests

--- JsTester/test.js
+++ JsTester/test.js
@@ -3,6 +3,13 @@ log(`pm.response.status::>${pm.response.status}<`);
 const data = pm.response.json();
 
 log(data);
-pm.test('this is a test ',() => {
-   pm.expect(pm.response.code).to.equal(200); 
+pm.test('this is a test ', () => {
+    pm.expect(pm.response.code).to.equal(200);
+    pm.expect(data).to.be.an('object');
+
+    Object.keys(data).forEach(function (date) {
+        pm.expect(date).to.match(/^\\d{4}-\\d{2}-\\d{2}$/, "Date format is not 'YYYY-MM-DD");
+        pm.expect(date.substring(0, 4)).to.eq('2024');
+    });
+
 });
--- PostMan.Net.Model/net/jsTest/Pm.cs
+++ PostMan.Net.Model/net/jsTest/Pm.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 using RestSharp;
 using Jint.Native;
 using Jint.Runtime;
@@ -18,6 +21,7 @@ public class Pm
     private object _subject;
 
     private object _expectation;
+
     public Pm Expect(object subject)
     {
         _subject = subject;
@@ -27,6 +31,60 @@ public class Pm
     public Pm To
     {
         get { return this; }
+    }
+
+    private bool _typeCheck;
+
+    public Pm Be
+    {
+        get
+        {
+            _typeCheck = true;
+            return this;
+        }
+    }
+
+    public Pm An(string type)
+    {
+        bool ok = false;
+        
+        switch (type)
+        {
+            case "int":
+            {
+                ok = _subject is double d && (Math.Round(d) - d == 0.0d);
+                break;
+            }
+            case "string":
+            {
+                ok = _subject is string;
+                break;
+            }
+            case "double":
+            {
+                ok = _subject is double || _subject is decimal || _subject is float;
+                break;
+            }
+            case "object":
+            {
+                ok = !_subject.GetType().IsValueType;
+                if (_subject is JsonElement json)
+                {
+                    ok = ok || json.ValueKind == JsonValueKind.Object;
+                }
+
+                break;
+            }
+        }
+        
+        if (ok)
+        {
+            Console.WriteLine("type is OK");
+            return this;
+        }
+
+        Console.Error.WriteLine($"type error. expecting {type}. found {_subject.GetType().Name}");
+        return this;
     }
 
     public Pm Equal(object expectation)
@@ -39,8 +97,25 @@ public class Pm
         {
             Console.Error.WriteLine("ERROR ! ");
         }
+
         return this;
     }
+
+    public Pm Match(string regex, string message)
+    {
+        Regex r = new Regex(regex);
+        var match = r.Match(_subject.ToString());
+        if (match.Success)
+        {
+            Console.WriteLine("regew match OK");
+        }
+        else
+        {
+            Console.WriteLine(message+ "  -  "+_subject.ToString());
+        }
+        return this;
+    }
+        
 
     public int Value { get; set; } = 1789;
 
